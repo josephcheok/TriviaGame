ee2dc9dff4b9b01c428da6c0719e3c7cd7d5fc1d
josephcheok <joseph@cheokandco.com>
2019-07-18 00:04:50 +1000

    Object quizzes created

---
+++ assets/javascript/app.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+  var quizzes = {
+    Q1: {
+      Question:
+        "Which war movie won the Academy Award for Best Picture in 2009?",
+      A: "The Hurt Locker",
+      B: "Saving Private Ryan",
+      C: "Letters to Iwojima",
+      D: "Inglorious Basterds"
+    },
+    Q2: {
+      Question:
+        "What was the name of the second Indiana Jones movie, released in 1984?",
+      A: "Indiana Jones and the Kingdom of the Crystal Skull",
+      B: "Raiders of the Lost Ark",
+      C: "Indiana Jones and the Temple of Doom",
+      D: "Indiana Jones and the Last Crusade"
+    },
+    Q3: {
+      Question: "Which actress plays Katnis Everdeen in the Hunger Games?",
+      A: "Jennifer Gardner",
+      B: "Jennifer Lawrence",
+      C: "Jennifer Aniston",
+      D: "Jennifer Love Hewitt"
+    },
+    Q4: {
+      Question:
+        "Which English director was responsible for the epic movie Gladiator in 2000?",
+      A: "Peter Jackson",
+      B: "Ridley Scott",
+      C: "Ron Howard",
+      D: "Wolfgang Peterson"
+    },
+    Q5: {
+      Question:
+        "What movie featured humans living in a simulated world to keep their sleeping body functioning as batteries for machines in the real world?",
+      A: "The Truth",
+      B: "The Matrix",
+      C: "Gattaca",
+      D: "The Island"
+    },
+    Q6: {
+      Question:
+        "For what movie did Leonardo DiCaprio finally get an Academy Award for since his debut in Titanic in 1997?",
+      A: "The Aviator",
+      B: "Inception",
+      C: "The Wolf of Wall Street",
+      D: "The Revenant"
+    },
+    Q7: {
+      Question:
+        " How many movies did Brad Pitt and Angelina Jolie star in together?",
+      A: 1,
+      B: 2,
+      C: 3,
+      D: 4
+    },
+    Q8: {
+      Question: " “You had me at hello” was a famous line from which movie?",
+      A: "Forrest Gump",
+      B: "Jerry McGuire",
+      C: "When Harry met Sally",
+      D: "The Notebook"
+    },
+    Q9: {
+      Question:
+        "How many Spiderman movies are there since the release of Spider-Man in 2002?",
+      A: 5,
+      B: 6,
+      C: 8,
+      D: 10
+    },
+    Q10: {
+      Question: "Which of the following actors did not play Batman?",
+      A: "Michael Keaton",
+      B: "Adam East",
+      C: "Val Kilmer",
+      D: "Christian Bale"
+    }
+  };
+});
